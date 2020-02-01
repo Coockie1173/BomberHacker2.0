a0b8c2bc9898e785c4607cd162886844fe4ed5a2
Coockie1173 <rudjerud@hotmail.com>
2020-02-01 15:18:20 +0100

    It finally fucking works.

---
+++ BHackerOverhaul.3DData/DLObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BHackerOverhaul._3DData
+{
+    public class DLObject
+    {
+        public int DataOffset { get; set; }
+        public int Length { get; set; }
+
+        public List<Connection> connections;
+
+        public DLObject()
+        {
+            connections = new List<Connection>();
+        }
+    }
+}
--- BHackerOverhaul.DLHandling/DLParser.cs
+++ BHackerOverhaul.DLHandling/DLParser.cs
@@ -12,97 +12,125 @@ namespace BHackerOverhaul.DLHandling
     {
         public string[] GetParsedObject(byte[] Data)
         {
-           
-            int[] Headers = new HeaderReader().ReadOffsets(Data);
-            List<string> OutPut = new List<string>();
             List<string> V = new List<string>();
             List<string> F = new List<string>();
-            int AdditionalFOffset = 0;
+            List<string> OutPut = new List<string>();
+            int[] Headers = new HeaderReader().ReadOffsets(Data);
+            int AdditionalOffset = 1;
+            List<DLObject> Temp = new List<DLObject>();
 
             try
             {
-                foreach(int CurHeader in Headers)
+                foreach (int Offset in Headers)
                 {
-                    int CurPos = CurHeader;
-                    int GeometryOffset = 0;
-                    List<Connection> Con = new List<Connection>();
-          
-                    while(Data[CurPos] != 0xB8)
+                    int CurOffset = Offset;
+                    DLObject Buf = new DLObject();
+                    try
                     {
-                        if(Data[CurPos] == 0x04)
+                        while (Data[CurOffset] != 0xB8)
                         {
-                            GeometryOffset = CurPos;
-                        }
-                        else if (Data[CurPos] == 0xB1)
-                        {
-                            Connection c = new Connection();
-                            c.Connection1 = Data[CurPos + 1] / 2 + 1;
-                            c.Connection2 = Data[CurPos + 2] / 2 + 1;
-                            c.Connection3 = Data[CurPos + 3] / 2 + 1;
-                            Con.Add(c);
-                            c = new Connection();
-                            c.Connection1 = Data[CurPos + 5] / 2 + 1;
-                            c.Connection2 = Data[CurPos + 6] / 2 + 1;
-                            c.Connection3 = Data[CurPos + 7] / 2 + 1;
-                            Con.Add(c);
-                        }
-                        else if(Data[CurPos] == 0xBF)
-                        {
-                            Connection c = new Connection();
-                            c.Connection1 = Data[CurPos + 5] / 2 + 1;
-                            c.Connection2 = Data[CurPos + 6] / 2 + 1;
-                            c.Connection3 = Data[CurPos + 7] / 2 + 1;
-                        }
-                        CurPos += 0x08;
-                    }
-                    byte[] temp;
-                    string BinLength = Convert.ToString(Data[GeometryOffset + 2], 2).PadLeft(8, '0');
-                    BinLength = BinLength.Substring(0, 6);
-                    int Length = Convert.ToInt32(BinLength, 2);
-                    temp = new byte[4];
-                    Array.Copy(Data, GeometryOffset + 4, temp, 0,4);
-                    temp[0] = 0;
-                    if(BitConverter.IsLittleEndian)
-                    {
-                        Array.Reverse(temp);
-                    }
-                    GeometryOffset = BitConverter.ToInt32(temp,0);
-
-                    foreach (Connection c in Con)
-                    {
-                        string Builder = "f ";
-                        Builder += $"{c.Connection1 + AdditionalFOffset} {c.Connection2 + AdditionalFOffset} {c.Connection3 + AdditionalFOffset}";
-                        F.Add(Builder);
-                    }
-
-                    for (int i  = 0; i < Length; i++)
-                    {
-                        string Builder = "v ";
-                        int buf = 0;
-                        temp = new byte[2];
-                        while(buf != 6)
-                        {
-                            Array.Copy(Data, GeometryOffset + buf, temp, 0, 2);
-                            if(BitConverter.IsLittleEndian)
+                            if (Data[CurOffset] == 0x04)
                             {
-                                Array.Reverse(temp);
-                            }
-                            Builder += BitConverter.ToInt16(temp,0).ToString() + " ";
-                            buf += 2;
-                        }
-                        V.Add(Builder);
-                        GeometryOffset += 0x10;
+                                Temp.Add(Buf);
+                                Buf = new DLObject();
+                                byte[] Pos = new byte[4];
+                                Array.Copy(Data, CurOffset + 4, Pos, 0, 4);
+                                Pos[0] = 0;
+                                if(BitConverter.IsLittleEndian)
+                                {
+                                    Array.Reverse(Pos);
+                                }
+                                Buf.DataOffset = BitConverter.ToInt32(Pos, 0);
 
-                        AdditionalFOffset++;
+                                string Length = Convert.ToString(Data[CurOffset + 2], 2).PadLeft(8, '0');
+                                Length = Length.Substring(0, 6);
+                                Buf.Length = Convert.ToInt32(Length, 2);
+                            }
+                            else if (Data[CurOffset] == 0xB1)
+                            {
+                                Connection c = new Connection();
+                                c.Connection1 = Data[CurOffset + 1] / 2;
+                                c.Connection2 = Data[CurOffset + 2] / 2;
+                                c.Connection3 = Data[CurOffset + 3] / 2;
+                                Buf.connections.Add(c);
+
+                                c = new Connection();
+                                c.Connection1 = Data[CurOffset + 5] / 2;
+                                c.Connection2 = Data[CurOffset + 6] / 2;
+                                c.Connection3 = Data[CurOffset + 7] / 2;
+                                Buf.connections.Add(c);
+                            }
+                            else if (Data[CurOffset] == 0xBF)
+                            {
+                                Connection c = new Connection();
+                                c.Connection1 = Data[CurOffset + 5] / 2;
+                                c.Connection2 = Data[CurOffset + 6] / 2;
+                                c.Connection3 = Data[CurOffset + 7] / 2;
+                                Buf.connections.Add(c);
+                            }
+
+                            CurOffset += 0x08;
+                        }
+                        Temp.Add(Buf);
+                    }
+                    catch(Exception ex)
+                    {
+
+                    }
+                }
+
+                foreach (DLObject obj in Temp)
+                {
+                    foreach (Connection connection in obj.connections)
+                    {
+                        string builder = "f ";
+                        builder += $"{connection.Connection1 + AdditionalOffset} {connection.Connection2 + AdditionalOffset} {connection.Connection3 + AdditionalOffset}";
+                        F.Add(builder);
                     }
 
-                    
+                    int CurOffset = obj.DataOffset;
+
+                    for (int i = 0; i < obj.Length; i++)
+                    {
+                        Vector3 buf = new Vector3();
+                        byte[] temp = new byte[2];
+                        Array.Copy(Data, CurOffset, temp, 0, 2);
+                        if (BitConverter.IsLittleEndian)
+                        {
+                            Array.Reverse(temp);
+                        }
+                        buf.X = (float)BitConverter.ToInt16(temp, 0);
+                        temp = new byte[2];
+                        Array.Copy(Data, CurOffset + 2, temp, 0, 2);
+                        if (BitConverter.IsLittleEndian)
+                        {
+                            Array.Reverse(temp);
+                        }
+                        buf.Y = (float)BitConverter.ToInt16(temp, 0);
+                        temp = new byte[2];
+                        Array.Copy(Data, CurOffset + 4, temp, 0, 2);
+                        if (BitConverter.IsLittleEndian)
+                        {
+                            Array.Reverse(temp);
+                        }
+                        buf.Z = (float)BitConverter.ToInt16(temp, 0);
+
+                        string Builder = "v ";
+                        Builder += buf.ToString();
+
+                        V.Add(Builder);
+
+                        AdditionalOffset++;
+                        CurOffset += 0x10;
+                    }
                 }
+
             }
-            catch(Exception e)
+            catch (Exception ex)
             {
 
             }
+
             OutPut.AddRange(V);
             OutPut.AddRange(F);
             return OutPut.ToArray();
